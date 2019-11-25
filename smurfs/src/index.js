7327908463e1502efa096c8dca950a284ef973ab
Tyree Lebeau <tyreelebeau@Tyrees-iMac.local>
2019-11-24 22:17:36 -0600

    mvp complete

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import rootReducer from "./reducers";
+
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+<App />
+    </Provider>
+,
+ document.getElementById("root"));
